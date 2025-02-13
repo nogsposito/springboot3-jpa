4427f1b333b01c38678cbb1d61e05527511328e2
nogsposito <vsan@cesar.school>
2025-02-13 20:23:39 -0300

    product criado

--- course/src/main/java/com/educandoweb/course/entities/Product.java
+++ course/src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.entities;
 
-import java.beans.Transient;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,23 +9,31 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private Double price;
+    private String imgURL;
 
-    private Set<Product> products = new HashSet<>();
+    @Transient
+    private Set<Category> categories = new HashSet<>(); // conjunto: nao tem mais que um tipo
 
-    public Category(){}
+    public Product(){}
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name, String description, Double price, String imgURL) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgURL = imgURL;
     }
 
     public Long getId() {
@@ -45,9 +52,32 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    @Transient
-    public Set<Product> getProducts() {
-        return products;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     @Override
@@ -66,7 +96,7 @@ public class Category implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Category other = (Category) obj;
+        Product other = (Product) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
@@ -74,5 +104,5 @@ public class Category implements Serializable {
             return false;
         return true;
     }
-    
+
 }
